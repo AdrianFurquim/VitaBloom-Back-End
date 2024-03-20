f1227e964fafaa6139e476d13562fbd6be47d7f3
AdrianFurquim <adrian.furquim26@gmail.com>
2024-03-20 16:28:57 -0300

    Adicionando PUT Produtos

--- src/main/java/vita/bloom/front/end/controller/ControllerProduto.java
+++ src/main/java/vita/bloom/front/end/controller/ControllerProduto.java
@@ -2,6 +2,7 @@ package vita.bloom.front.end.controller;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,4 +44,21 @@ public class ControllerProduto {
             return "ID do produto não encontrado, insira outro ID porfavor";
         }
     }
+    
+    @PutMapping("/vitabloom/produto/editar/{id}")
+    public Produto editarProduto(@PathVariable("id") Long idProduto, @RequestBody Produto produtoAtualizado){
+        Optional<Produto> produtoExistenteOptional = produtosRepository.findById(idProduto);
+        if (produtoExistenteOptional.isPresent()) {
+        Produto produtoExistente = produtoExistenteOptional.get();
+        produtoExistente.setNomeProduto(produtoAtualizado.getNomeProduto());
+        produtoExistente.setValorProduto(produtoAtualizado.getValorProduto());
+        produtoExistente.setDescricaoProduto(produtoAtualizado.getDescricaoProduto());
+        // Defina outros campos conforme necessário
+        
+        return produtosRepository.save(produtoExistente);
+    } else {
+        return null; 
+    }
+}
+
 }
